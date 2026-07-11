f17593093758103b3ae231751ac0cedb329aa03f
denshoelacee <sgywkdn@gmail.com>
2026-07-11 17:32:01 +0800

    earnings

--- routes/AppRoute.tsx
+++ routes/AppRoute.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import BaseLayout from "../layouts/BaseLayout";
 import Login from "../features/auth/Login"
 import Home from "../features/dashboard/Home";
+<<<<<<< Updated upstream
 import Profile from "../features/profile/Profile";
 import Support from "../features/support/Support"
 import Theme from "../features/theme/Theme"
@@ -21,6 +22,20 @@ export default function AppRoutes() {
         <Route path="support" element={<Support />} />
         <Route path="earnings" element={<Earnings/>}/>
       </Route>
+=======
+import Support from "../features/support/Support"
+export default function AppRoutes() { 
+
+  return ( 
+    <Routes> 
+      {/* Main Website */} 
+      <Route path="/"  element={<BaseLayout />}> 
+          <Route index element={<Login />} />
+          <Route path="dashboard" element={<Home/>}/>
+                    <Route path="support" element={<Support/>}/>
+
+      </Route> 
+>>>>>>> Stashed changes
 
       {/* Protected Routes */}
 
